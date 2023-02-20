bfd7a9ee797a5a720583bfcfce22489d11bba8db
MarlyasDad <yudaev.alex@gmail.com>
2023-02-20 15:25:23 +0300

    Update text-split test

--- text_tools.py
+++ text_tools.py
@@ -26,11 +26,18 @@ async def split_by_words(morph, text):
 def test_split_by_words():
     # Экземпляры MorphAnalyzer занимают 10-15Мб RAM т.к. загружают в память много данных
     # Старайтесь организовать свой код так, чтоб создавать экземпляр MorphAnalyzer заранее и в единственном числе
-    morph = pymorphy2.MorphAnalyzer()
+    async def do_test():
+        morph = pymorphy2.MorphAnalyzer()
 
-    assert split_by_words(morph, 'Во-первых, он хочет, чтобы') == ['во-первых', 'хотеть', 'чтобы']
+        test_string = 'Во-первых, он хочет, чтобы'
+        assert await split_by_words(morph, test_string) == [
+            'во-первых', 'хотеть', 'чтобы']
 
-    assert split_by_words(morph, '«Удивительно, но это стало началом!»') == ['удивительно', 'это', 'стать', 'начало']
+        test_string = '«Удивительно, но это стало началом!»'
+        assert await split_by_words(morph, test_string) == [
+            'удивительно', 'это', 'стать', 'начало']
+
+    asyncio.run(do_test())
 
 
 def calculate_jaundice_rate(article_words, charged_words):
